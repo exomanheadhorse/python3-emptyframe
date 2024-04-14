60ccfb999b352c8e3b5717e9b28627f4c3c63dae
luka <799681071@qq.com>
2024-04-14 19:19:57 +0800

    1

--- task/task.py
+++ task/task.py
@@ -1,6 +1,14 @@
-class TaskMgr(object):
+import subprocess
+from typing import Dict
+import src.common as common
+
+
+class Task(object):
     def __init__(self, **args) -> None:
-        self.task_id: int = args.get('task_id', 0)
+        self.task_id: str = args.get('task_id', '')
+        self.target_viscosity: float = args.get('target_viscosity', 0)
+        self.temperature: int = args.get('temperature', 0)
+        self.coals_content = args.get('coals_content', {})
         self.task_name: str = args.get('task_name', '')
         self.task_status: int = args.get('task_status', 0)
         self.create_time: str = args.get('create_time', '')
@@ -14,14 +22,21 @@ class TaskMgr(object):
     def update_status_in_db(self, status: int) -> None:
         pass
 
-    def create_task(self, task_id, ) -> None:
-        self.task_id = task_id
+    def start(self) -> None:
+        subprocess.run(["python3", "optimization.py", ], stdout=)
         pass
 
 
-class Task:
+class TaskMgr:
     def __init__(self):
-        pass
+        self.tasks: Dict[str, Task] = {}
+        self.db_ins = common.get_db_hander("task_info")
 
     def start(self) -> None:
+        pass
+
+    def create_task(self):
+        pass
+
+    def query_task(self, task_id: str) -> Task:
         pass
